0b4f6e9d0af124511ec793d98243265992ad9d1a
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2020-05-22 01:47:47 -0700

    Session and log in done

--- application.py
+++ application.py
@@ -1,12 +1,21 @@
 import os
 import csv
 
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request, redirect, url_for, g
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+
+class User:
+    def __init__(self, id, user_name, email, password):
+        self.id = id
+        self.user_name = user_name
+        self.email = email
+        self.password = password
+
 
 # Check for environment variable
 if not os.getenv("MYDATAONHEROKU"):
@@ -17,9 +26,23 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
+
 # Set up database
 engine = create_engine(os.getenv("MYDATAONHEROKU"))
 db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'id' in session:
+        users = db.execute('select id, user_name, email, password from users where id = :id ',
+                           {'id': session['id']}).fetchall()
+        db_user = users[0]
+        user = User(db_user.id, db_user.user_name, db_user.email, db_user.password)
+        g.user = user
+
 
 
 @app.route("/")
@@ -27,19 +50,27 @@ def index():
     return render_template("signinorup.html")
 
 
+@app.route('/signout', methods=['POST', 'GET'])
+def signout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
 @app.route('/signin', methods=['POST', 'GET'])
 def signin():
 
     if request.method == 'POST':
+        session.pop('id', None)
         email = request.form.get('email')
         password = request.form.get('password')
 
-        users = db.execute('select email, password from users where email = :email and password = :password',
+        users = db.execute('select id, email, password from users where email = :email and password = :password',
                            {'email': email, 'password': password}).fetchall()
 
         if len(users) == 0:
             return redirect(url_for('badusernameorpw'))
         else:
+            session['id'] = users[0].id
             return redirect(url_for('search'))
     else:
         return render_template("signin.html")
@@ -70,6 +101,8 @@ def signup():
 # will check if there is a user name and password matched with this in the database and give access
 @app.route('/search', methods=['POST', 'GET'])
 def search():
+    if not g.user:
+        return redirect(url_for('index'))
     return render_template('search.html')
 
 
@@ -80,8 +113,10 @@ def display():
 
 @app.route('/selectedbooks', methods=['POST'])
 def selected_books():
-    search_results = request.form.get('search')
-    books = db.execute('select isbn, author, title, yearr from books where isbn like :isbn or author = :author or title = :title',
+    if not g.user:
+        return redirect(url_for('index'))
+    search_results = '%' + request.form.get('search') +'%'
+    books = db.execute('select isbn, author, title, yearr from books where isbn like :isbn or author like :author or title like :title',
                        {'yearr': search_results,'isbn': search_results, 'title': search_results, 'author': search_results}).fetchall()
     if len(books) == 0:
         return redirect(url_for('oops'))
@@ -91,6 +126,8 @@ def selected_books():
 
 @app.route('/bookdetail/<string:title>/<string:author>/<string:isbn>/<string:yearr>', methods=['POST', 'GET'])
 def bookdetail(title, author, isbn, yearr):
+    if not g.user:
+        return redirect(url_for('index'))
     return render_template('bookdetail.html', title=title, author=author, isbn=isbn, yearr=yearr)
 
 
